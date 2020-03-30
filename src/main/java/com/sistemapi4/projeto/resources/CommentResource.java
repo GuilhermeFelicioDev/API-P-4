3bfc3ae0c23865b24c370617ccc6df985e03aae6
Guilherme Felicio <gfcwarlord@gmail.com>
2020-03-29 23:34:30 -0300

    CRUD implementado

--- src/main/java/com/sistemapi4/projeto/resources/CommentResource.java
+++ src/main/java/com/sistemapi4/projeto/resources/CommentResource.java
@@ -9,14 +9,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.CommentMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.sistemapi4.projeto.entities.Comment;
 import com.sistemapi4.projeto.entities.Comment;
 import com.sistemapi4.projeto.services.CommentService;
 
