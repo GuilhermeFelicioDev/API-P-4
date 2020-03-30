616d264b90a66a54d6afa6e75aa8472dc3562e38
Guilherme Felicio <gfcwarlord@gmail.com>
2020-03-29 23:33:43 -0300

    CRUD implementado

--- src/main/java/com/sistemapi4/projeto/services/CommentService.java
+++ src/main/java/com/sistemapi4/projeto/services/CommentService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sistemapi4.projeto.entities.Comment;
+import com.sistemapi4.projeto.entities.Post;
 import com.sistemapi4.projeto.repositories.CommentRepository;
 
 @Service
@@ -22,5 +23,21 @@ public class CommentService {
 	public Comment findById(Long id) {
 		Optional<Comment> obj = repository.findById(id);
 		return obj.get();
+	}
+	public Comment insert(Comment obj) {
+		return repository.save(obj);	
+	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	public Comment update(Long id, Comment obj) {
+		Comment entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(Comment entity, Comment obj) {
+		entity.setText(obj.getText());
 	}
 }
--- src/main/java/com/sistemapi4/projeto/services/PostService.java
+++ src/main/java/com/sistemapi4/projeto/services/PostService.java
@@ -1,12 +1,21 @@
 package com.sistemapi4.projeto.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sistemapi4.projeto.entities.Post;
+import com.sistemapi4.projeto.entities.User;
 import com.sistemapi4.projeto.repositories.PostRepository;
 
 @Service
@@ -23,4 +32,25 @@ public class PostService {
 		Optional<Post> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+	public Post insert(Post obj) {
+		return repository.save(obj);	
+	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	public Post update(Long id, Post obj) {
+		Post entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(Post entity, Post obj) {
+		entity.setTitle(obj.getTitle());
+		entity.setBody(obj.getBody());
+}
+
+	
+	
 }
